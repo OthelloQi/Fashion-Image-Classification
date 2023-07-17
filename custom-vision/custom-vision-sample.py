ef90a7c4cf3959ba56dd6a1003f9b544c66a8bfa
OthelloQi <112239793+OthelloQi@users.noreply.github.com>
2023-07-17 18:57:16 +0000

    net: sctp: fix NULL pointer dereference in socket destruction

--- custom-vision/custom-vision-sample.py
+++ custom-vision/custom-vision-sample.py
@@ -6,4 +6,11 @@
 import httplib, urllib, base64, json
  
 customvisionapi_key = '<<prediction key here>>'
-uri_base = 'southcentralus.api.
+uri_base = 'southcentralus.api.cognitive.microsoft.com'
+headers = {
+    'Prediction-Key': customvisionapi_key,
+    'Content-Type': 'application/json'
+}
+ 
+
+body = "{'Url':
