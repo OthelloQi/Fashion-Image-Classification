9c0ae7059d763e4f2f8fd65b58d1ea17ebd8fa03
OthelloQi <112239793+OthelloQi@users.noreply.github.com>
2023-08-24 17:23:59 +0000

    mmc: sh-mmcif: avoid oops on spurious interrupts (second try)

--- deep-learning/cnn_setup_changes.py
+++ deep-learning/cnn_setup_changes.py
@@ -1,4 +1,7 @@
 model = Sequential()
 
 model.add(Conv2D(64, kernel_size=(3,3), padding = 'same', activation = 'relu', input_shape=input_shape))
-mode
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+model.add(Conv2D(32, kernel_size=(3,3), padding = 'same', activat
